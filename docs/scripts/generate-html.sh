b50efbd51a633c6e55af013d1c4014aaa915e7d6
Shintaro Uchiyama <shintaro.a.uchiyama@gmail.com>
2022-02-15 23:15:42 +0900

    refacter: find all target file

--- docs/scripts/generate-html.sh
+++ docs/scripts/generate-html.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # Create output directories
 mkdir -p output/html
-find . -type d -not -path './output*' -not -path './scripts*' -not -path '.' -exec sh -c 'mkdir -p output/html/"${0}"' {} \;
+find . -type d -not -path './output*' -not -path './scripts*' -not -path '.' | xargs -I {} sh -c 'mkdir -p output/html/"${0#./}"' {}
 
 # convert from md to html
-find . -iname "index.md" -type f -exec sh -c 'docker run --rm --volume "`pwd`:/data" pandoc/latex:2.17.1 -s -t html5 -c github.css --metadata title="${0}" "${0}" -o "output/html/${0%.md}.html"' {} \;
+find . -name "*.md" -type f -not -path "*.template.md" -not -path "*README.md" | xargs -I {} sh -c 'echo "${0#./}"' {} | xargs -I {} sh -c 'docker run --rm --volume "`pwd`:/data" pandoc/latex:2.17.1 -s -t html5 -c github.css --metadata title="${0}" "${0}" -o "output/html/${0%.md}.html"' {}
 # copy base css file
 find output/html -type d | xargs -I {} cp github.css {}
 # copy image files
 find . -type f -name "*.svg" -not -path './output/*' | xargs -I {} cp "{}" "output/html/{}"
 
 # change md alink to html
-find output/html -type f -name "*.html" | xargs sed -i '' -e "s/index.md/index.html/g"
+find output/html -type f -name "*.html" | xargs sed -i '' -e "s/.md/.html/g"
--- docs/scripts/generate-mermaid-image.sh
+++ docs/scripts/generate-mermaid-image.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker pull minlag/mermaid-cli:8.13.10
-docker run -v "$PWD/design:/data" minlag/mermaid-cli:8.13.10 -i /data/index.template.md -o /data/index.md
+find . -name "*.template.md" -type f | xargs -I {} sh -c 'echo "${0#./}"' {} | xargs -I {} sh -c 'echo "${0%.template.md}"' {} | xargs -I {} sh -c 'docker run -v "$PWD:/data" minlag/mermaid-cli:8.13.10 -i /data/"${0}".template.md -o /data/"${0}.md"' {}
