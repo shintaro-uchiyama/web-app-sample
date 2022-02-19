469ebe3a7c2673faa9397fed3756a2de3fa75bd3
Shintaro Uchiyama <shintaro.a.uchiyama@gmail.com>
2022-02-19 20:49:57 +0900

    create dashboard by tailwind kit

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import './App.css';
-import Home from './components/pages/index'
-import Companies from './components/pages/companies/index'
+import React from "react";
+import "./App.css";
+import Home from "./components/pages/index";
+import Companies from "./components/pages/companies/index";
 import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="bg-teal-50 text-3xl font-bold underline">Welcome to React Router!</h1>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="companies" element={<Companies />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
---
+++ src/web/src/components/parts/navigations/SideMenu/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function SideMenu() {
+  let navClass =
+    "w-36 max-w-full bg-blue-800 h-screen flex flex-col text-white fixed lg:absolute lg:sticky top-0 transition-transform transform duration-500 ease";
+  navClass += " -translate-x-full lg:translate-x-0";
+  return <></>;
+}
+
+export default SideMenu;
