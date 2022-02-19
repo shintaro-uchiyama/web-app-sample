c9abcc52f7400237022f5eb2dd5be42bd1dfa0a8
Shintaro Uchiyama <shintaro.a.uchiyama@gmail.com>
2022-02-19 21:22:57 +0900

    separate sidemenu and navbar

--- src/web/src/components/parts/navigations/SideMenu/index.tsx
+++ src/web/src/components/parts/navigations/SideMenu/index.tsx
@@ -1,10 +1,63 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBuilding,
+  faHome,
+  faCalendar,
+} from "@fortawesome/free-solid-svg-icons";
 
 function SideMenu() {
-  let navClass =
-    "w-36 max-w-full bg-blue-800 h-screen flex flex-col text-white fixed lg:absolute lg:sticky top-0 transition-transform transform duration-500 ease";
-  navClass += " -translate-x-full lg:translate-x-0";
-  return <></>;
+  return (
+    <div className="h-screen hidden lg:block shadow-lg relative w-60">
+      <div className="bg-white h-full dark:bg-gray-700">
+        <div className="flex items-center justify-start pt-6 ml-8">
+          <p className="font-bold dark:text-white text-xl">Ucwork</p>
+        </div>
+        <nav className="mt-6">
+          <div>
+            <a
+              className="w-full text-gray-800 dark:text-white flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start border-l-4 border-purple-500"
+              href="#"
+            >
+              <span className="text-left">
+                <FontAwesomeIcon
+                  icon={faHome}
+                  fill="currentColor"
+                  className=""
+                />
+                <span className="mx-4 text-sm font-normal">Home</span>
+              </span>
+            </a>
+            <a
+              className="w-full text-gray-400 flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start hover:text-gray-800 border-l-4 border-transparent"
+              href="#"
+            >
+              <span className="text-left">
+                <FontAwesomeIcon
+                  icon={faBuilding}
+                  fill="currentColor"
+                  className=""
+                />
+              </span>
+              <span className="mx-4 text-sm font-normal">Company</span>
+            </a>
+            <a
+              className="w-full text-gray-400 flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start hover:text-gray-800 border-l-4 border-transparent"
+              href="#"
+            >
+              <span className="text-left">
+                <FontAwesomeIcon
+                  icon={faCalendar}
+                  fill="currentColor"
+                  className=""
+                />
+              </span>
+              <span className="mx-4 text-sm font-normal">Schedule</span>
+            </a>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default SideMenu;
