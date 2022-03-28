45fdc94e10a8b776607747395101d24e0e0e5aa0
Shintaro Uchiyama <shintaro.a.uchiyama@gmail.com>
2022-03-29 07:35:18 +0900

    genereate

--- src/sales/main.go
+++ src/sales/main.go
@@ -1,33 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"log"
 
-	"github.com/gin-gonic/gin"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	sw "github.com/shintaro-uchiyama/web-app-sample/sales/cmd/api-document/out/go/go"
 )
 
 func main() {
-	fmt.Println("Hello golang from docker!")
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		dsn := fmt.Sprintf(
-			"host=sales-db user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Tokyo",
-			os.Getenv("POSTGRES_USER"),
-			os.Getenv("POSTGRES_PASSWORD"),
-			os.Getenv("POSTGRES_DB"),
-		)
-		_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-		if err != nil {
-			c.JSON(500, gin.H{
-				"message": fmt.Sprintf("db init error: %s", err),
-			})
-		}
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run()
+	log.Printf("Server started")
+
+	router := sw.NewRouter()
+
+	log.Fatal(router.Run(":8080"))
 }
