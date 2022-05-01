6e9a6dc96b48271f5ca9871431be4feabf5c738f
Shintaro Uchiyama <shintaro.a.uchiyama@gmail.com>
2022-05-01 20:04:22 +0900

    feat: add canvas

--- src/web/src/components/pages/draw/index.tsx
+++ src/web/src/components/pages/draw/index.tsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useRef } from "react";
 
 export const Draw = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const canvas = canvasRef.current;
+  if (canvas !== null) {
+    const context = canvas.getContext("2d");
+    if (context !== null) {
+      context.fillStyle = "#E0FFFF";
+      context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
         <p>draw</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <canvas ref={canvasRef} />
       </header>
     </div>
   );
