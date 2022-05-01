36e29f34fe351c4ac126373b9b17d98b6ff13adf
Shintaro Uchiyama <shintaro.a.uchiyama@gmail.com>
2022-05-01 20:10:31 +0900

    feat: make canvas circle

--- src/web/src/components/pages/draw/index.tsx
+++ src/web/src/components/pages/draw/index.tsx
@@ -1,15 +1,24 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 
 export const Draw = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const canvas = canvasRef.current;
-  if (canvas !== null) {
-    const context = canvas.getContext("2d");
-    if (context !== null) {
-      context.fillStyle = "#E0FFFF";
-      context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+  const draw = (ctx: CanvasRenderingContext2D) => {
+    ctx.fillStyle = "#E0FFFF";
+    ctx.beginPath();
+    ctx.arc(50, 100, 20, 0, 2 * Math.PI);
+    ctx.fill();
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas !== null) {
+      const context = canvas.getContext("2d");
+      if (context !== null) {
+        draw(context);
+      }
     }
-  }
+  }, [draw]);
+
   return (
     <div className="App">
       <header className="App-header">
