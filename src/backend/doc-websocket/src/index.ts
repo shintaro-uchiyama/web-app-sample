0c1d510ce7f1dc2c7bf3a085120607a479106efa
Shintaro Uchiyama <shintaro.a.uchiyama@gmail.com>
2022-08-20 16:45:35 +0900

    feat: add server

--- src/backend/doc-websocket/src/index.ts
+++ src/backend/doc-websocket/src/index.ts
@@ -1,1 +1,15 @@
+import http from "http";
+
+const host: string = "localhost";
+const port: number = 1234;
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("okay");
+});
+
+server.listen(port, host, () => {
+  console.log(`running at '${host}' on port ${port}`);
+});
+
 console.log("Hello world!!!");
