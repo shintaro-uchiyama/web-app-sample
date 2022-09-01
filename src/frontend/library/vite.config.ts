55a9dc5dba4dd088872e559aed0a0b124ecbdb8f
Shintaro Uchiyama <shintaro.a.uchiyama@gmail.com>
2022-09-01 22:17:07 +0900

    fix: absolute path

--- src/frontend/library/vite.config.ts
+++ src/frontend/library/vite.config.ts
@@ -1,10 +1,16 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
+  },
+  resolve: {
+    alias: {
+      "/": path.resolve(__dirname, "src"),
+    },
   },
 });
