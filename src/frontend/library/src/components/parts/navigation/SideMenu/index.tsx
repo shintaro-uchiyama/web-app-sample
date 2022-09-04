55f7492ce506356d58ae7b1caa47b1600cef04a9
Shintaro Uchiyama <shintaro.a.uchiyama@gmail.com>
2022-09-04 23:29:12 +0900

    sidemenu color

--- src/frontend/library/src/components/parts/navigation/SideMenu/index.tsx
+++ src/frontend/library/src/components/parts/navigation/SideMenu/index.tsx
@@ -1,21 +1,24 @@
 import { faBook } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link, useLocation } from "react-router-dom";
+import { sideMenuBackgroundColor, titleTextColor } from "~/styles/colors";
 
 const SideMenu = () => {
   const location = useLocation();
   return (
-    <div className="h-screen hidden lg:block shadow-lg relative w-32">
-      <div className="bg-white h-full">
+    <div
+      className={`h-screen hidden lg:block shadow-lg relative w-32 ${sideMenuBackgroundColor}`}
+    >
+      <div className="h-full">
         <div className="flex items-center justify-start pt-6 ml-4">
-          <p className="font-bold text-xl">Library</p>
+          <p className={`font-bold text-xl ${titleTextColor}`}>Library</p>
         </div>
         <nav className="mt-6">
           <div>
             <Link
               className={`
                 w-full flex items-center pl-2 p-2 my-2 transition-colors duration-200 justify-start border-l-4 ${
-                  location.pathname === "/"
+                  location.pathname === "/memo"
                     ? "text-gray-800 border-purple-500"
                     : "text-gray-400 border-transparent hover:text-gray-800"
                 } 
--- src/frontend/library/src/styles/colors.ts
+++ src/frontend/library/src/styles/colors.ts
@@ -1,5 +1,7 @@
 export const baseBackgroundColor = "bg-white dark:bg-slate-800";
 
+export const sideMenuBackgroundColor = "bg-gray-200 dark:bg-slate-700";
+
 export const titleTextColor = "text-slate-900 dark:bg-slate-800";
 
 export const bodyTextColor = "text-slate-500 dark:text-slate-400";
