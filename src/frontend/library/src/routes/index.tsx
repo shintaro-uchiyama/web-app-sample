f2abb211d4171297832c9a3faec5b8792a26f86e
Shintaro Uchiyama <shintaro.a.uchiyama@gmail.com>
2022-09-04 23:07:46 +0900

    fix: use /memo page

--- src/frontend/library/src/routes/index.tsx
+++ src/frontend/library/src/routes/index.tsx
@@ -1,10 +1,10 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Memo from "~/pages/memo";
 
 const AppRoutes = () => (
   <Routes>
-    <Route path="/memo" element={<Memo />}></Route>
-    <Route path="/" element={<Memo />}></Route>
+    <Route path="/memo" element={<Memo />} />
+    <Route path="/" element={<Navigate replace to="/memo" />} />
   </Routes>
 );
 
