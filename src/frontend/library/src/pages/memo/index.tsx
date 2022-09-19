fb395653a2f695eaacda9de6f6bc563240121fbf
Shintaro Uchiyama <shintaro.a.uchiyama@gmail.com>
2022-09-19 11:16:19 +0900

    feat: collabo

--- src/frontend/library/src/pages/memo/index.tsx
+++ src/frontend/library/src/pages/memo/index.tsx
@@ -1,16 +1,25 @@
+import Collaboration from "@tiptap/extension-collaboration";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { WebsocketProvider } from "y-websocket";
+import * as Y from "yjs";
 import { bodyTextColor, menuBackgroundColor } from "~/styles/colors";
 
 const App = () => {
-  const content = [...Array(15)].reduce(
-    (prev, current) => `${prev}<p></p>`,
-    ""
+  const content = [...Array(15)].reduce((prev) => `${prev}<p></p>`, "");
+  const yDoc = new Y.Doc();
+  const provider = new WebsocketProvider(
+    "ws://docker-vm.local:1234",
+    "sample",
+    yDoc
   );
   const editor = useEditor({
     extensions: [
       StarterKit.configure({
         history: false,
+      }),
+      Collaboration.configure({
+        document: provider.doc,
       }),
     ],
     editorProps: {
