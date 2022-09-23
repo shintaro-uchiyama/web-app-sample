ddbc9b436b5ffaa1c78edb55a45d44af9dec9fb5
Shintaro Uchiyama <shintaro.a.uchiyama@gmail.com>
2022-09-23 23:32:44 +0900

    polling

--- src/frontend/http-client/index.js
+++ src/frontend/http-client/index.js
@@ -1,4 +1,30 @@
-const startPolling = async () => {
+let pollingIntervalID;
+
+const startPolling = () => {
+  const pollingStartButton = document.getElementById("polling-start-button");
+  pollingStartButton.setAttribute("disabled", "");
+
+  const pollingStopButton = document.getElementById("polling-stop-button");
+  pollingStopButton.removeAttribute("disabled");
+
+  const intervalMilliSecond = getIntervalMilliSecond();
+  pollingIntervalID = setInterval(() => {
+    httpRequest();
+  }, intervalMilliSecond);
+};
+
+const getIntervalMilliSecond = () => {
+  const pollingSpan = document.getElementById("polling-span");
+  const [hour, minute, second] = pollingSpan.value.split(":");
+  return (Number(hour) * 60 * 60 + Number(minute) * 60 + Number(second)) * 1000;
+};
+
+const httpRequest = async () => {
+  const pollingExecutionTimeElement = document.getElementById(
+    "polling-execution-time"
+  );
+  pollingExecutionTimeElement.textContent = nowString();
+
   const pollingURLElement = document.getElementById("polling-url");
   const pollingURL = pollingURLElement.value;
   console.log("pollingURL: ", pollingURL);
@@ -17,7 +43,7 @@ const startPolling = async () => {
     pollingResponseBodyElement.textContent = JSON.stringify(
       responseBody,
       null,
-      "\t"
+      4
     );
   } catch (error) {
     console.log("error: ", error);
@@ -27,5 +53,24 @@ const startPolling = async () => {
 };
 
 const stopPolling = () => {
-  console.log("stop polling");
+  const pollingStartButton = document.getElementById("polling-start-button");
+  pollingStartButton.removeAttribute("disabled");
+
+  const pollingStopButton = document.getElementById("polling-stop-button");
+  pollingStopButton.setAttribute("disabled", "");
+
+  clearInterval(pollingIntervalID);
+};
+
+const nowString = () => {
+  const now = new Date();
+  return `${now.getFullYear()}-${(now.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}-${now.getDate().toString().padStart(2, "0")} ${now
+    .getHours()
+    .toString()
+    .padStart(2, "0")}:${now.getMinutes().toString().padStart(2, "0")}:${now
+    .getSeconds()
+    .toString()
+    .padStart(2, "0")}`;
 };
