b2dcb8299ad3ca8618abc2af1d6e9b8ed0a9a28c
Shintaro Uchiyama <shintaro.a.uchiyama@gmail.com>
2022-09-24 11:57:39 +0900

    add login page

---
+++ src/frontend/http-client/login.js
@@ -0,0 +1,6 @@
+const login = () => {
+  const accountElement = document.getElementById("account-input");
+  const passwordElement = document.getElementById("password-input");
+  console.log("login");
+  console.log(accountElement.value, passwordElement.value);
+};
