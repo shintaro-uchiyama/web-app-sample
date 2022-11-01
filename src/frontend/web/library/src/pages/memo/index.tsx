39f1ef0cb42f7707967d4e554dde9dbcbc2a6f37
Shintaro Uchiyama <shintaro.a.uchiyama@gmail.com>
2022-11-01 23:59:25 +0900

    prev data

--- src/frontend/web/library/src/pages/memo/index.tsx
+++ src/frontend/web/library/src/pages/memo/index.tsx
@@ -1,71 +1,47 @@
-import Collaboration from "@tiptap/extension-collaboration";
-import CollaborationCursor from "@tiptap/extension-collaboration-cursor";
-import { Editor, EditorContent } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
-import { WebsocketProvider } from "y-websocket";
-import * as Y from "yjs";
-import { bodyTextColor, menuBackgroundColor } from "~/styles/colors";
-import "./styles.scss";
+import { useEffect, useRef, useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
-const App = () => {
-  const yDoc = new Y.Doc();
-  const provider = new WebsocketProvider(
-    "ws://docker-vm.local:1234",
-    "sample",
-    yDoc
-  );
-  // useEditor causes `Cannot read property 'matchesNode' of null` error
-  // So we use Editor class
-  const editor = new Editor({
-    extensions: [
-      StarterKit.configure({
-        history: false,
-      }),
-      Collaboration.configure({
-        document: provider.doc,
-        field: "prosemirror",
-      }),
-      CollaborationCursor.configure({
-        provider: provider,
-        user: {
-          name: getRandomName(),
-          color: getRandomColor(),
-        },
-      }),
-    ],
-    editorProps: {
-      attributes: {
-        class:
-          "prose prose-sm sm:prose lg:prose-lg xl:prose-2xl m-5 focus:outline-none h-96",
-      },
-    },
-  });
 
-  return (
-    <div
-      className={`rounded h-96 max-w-4xl mx-auto ${menuBackgroundColor} ${bodyTextColor}`}
-    >
-      <EditorContent className="editor__content" editor={editor} />
-    </div>
-  );
+const Memo = () => {
+  const [value, setValue] = useState('');
+
+  const usePrevious = (value: string) => {
+    const ref = useRef('');
+    useEffect(() => {
+      ref.current = value;
+    });
+    return ref.current;
+  };
+
+  const [date, setDate] = useState('');
+  const prev = usePrevious(date)
+  const callback = ()=>{
+     const now = new Date();
+     const today = `${now.getFullYear()}.${(now.getMonth()+1).toString().padStart(2,"0")}.${now.getDate().toString().padStart(2,"0")} ${now.getSeconds()}`
+     console.log('prev: ', prev)
+     console.log('date: ', date)
+     setDate(today)
+  }
+  const callbackRef = useRef<() => void>(callback);
+  useEffect(() => {
+    callbackRef.current = callback; // 新しいcallbackをrefに格納！
+  }, [callback]);
+
+  useEffect(()=>{
+    const tick = () => { callbackRef.current() } 
+    const id = setInterval(tick, 1000);
+    return () => {
+      clearInterval(id);
+    };
+  }, [])
+
+
+  return <ReactQuill theme="snow" value={value} onChange={setValue} >
+    <div style={{height: '600px'}}>{date}</div>
+  </ReactQuill>;
+
 };
 
-const getRandomName = (): string => {
-  const now = new Date();
 
-  return `Guest User ${now.getFullYear()}-${now
-    .getMonth()
-    .toString()
-    .padStart(2, "0")}-${now
-    .getDay()
-    .toString()
-    .padStart(
-      2,
-      "0"
-    )} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}.${now.getMilliseconds()}`;
-};
-
-const getRandomColor = (): string =>
-  "#" + Math.floor(Math.random() * 0xffffff).toString(16);
-
-export default App;
+export default Memo;
